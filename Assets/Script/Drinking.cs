bcc5544a96a3242b4326ddf9dd0671331cc6a3e0
Pedro3Lobo <pedrolobowork@gmail.com>
2021-12-24 18:29:14 +0000

    summary 24/12
    notification are working perfectly now just need to define a interval for the not

--- Assets/Script/Drinking.cs
+++ Assets/Script/Drinking.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Notifications.Android;
 
 public class Drinking : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class Drinking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Notification();
         tree1 = GameObject.Find("Tree1");
         tree2 = GameObject.Find("Tree2");
         tree3 = GameObject.Find("Tree3");
@@ -29,6 +31,7 @@ public class Drinking : MonoBehaviour
         cups_water++;
         drunk_water = GameObject.Find("Cups_Number").GetComponent<Text>();
         drunk_water.text = cups_water + "/11";
+        Notification();
         if (cups_water == 11)
         {
            
@@ -60,6 +63,38 @@ public class Drinking : MonoBehaviour
         int i = tree_n.GetComponent<treeBehavior>().stage;
         tree_n.GetComponent<treeBehavior>().stage = i + 1;
 
+    }
+    void Notification()
+    {
+        //Remove notifications channel to send   
+        AndroidNotificationCenter.CancelAllDisplayedNotifications();
+
+        // Create the android Notification Channel that send message throw
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Notification Channel",
+            Importance = Importance.Default,
+            Description = "Reminder to Drink water",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+        //Create the notifications that is created to be sent 
+        var notification = new AndroidNotification();
+        notification.Title = "Hey! Your plant needs water!";
+        notification.Text = "The plants needs water to survive.";
+        notification.FireTime = System.DateTime.Now.AddSeconds(15);
+
+        //Send the Notification
+        var id = AndroidNotificationCenter.SendNotification(notification, "channel_id");
+
+
+        // if the script  is run and a message is already scheduled, cancel it and re-schedule another message 
+        if (AndroidNotificationCenter.CheckScheduledNotificationStatus(id)==NotificationStatus.Scheduled)
+        {
+            AndroidNotificationCenter.CancelAllNotifications();
+            AndroidNotificationCenter.SendNotification(notification, "channel_id");
+        }
     }
 
 }
